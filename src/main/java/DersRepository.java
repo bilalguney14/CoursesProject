e2a31c251a9311da8ed0454629f1af5f2ed1778e
Muhterem Atla <muhterematla@gmail.com>
2023-03-21 15:35:32 +0300

    Connect

--- src/main/java/DersRepository.java
+++ src/main/java/DersRepository.java
@@ -1,3 +1,23 @@
-public class DersRepository {
+import java.sql.*;
 
+public class DersRepository {
+    private Connection conn;
+    private Statement st;
+    private PreparedStatement prst;
+    private void  getConnection(){
+        try {
+            this.conn=DriverManager.getConnection("jdbc:postgrsql://localhost:5432/jdbc_db","dev_user","password");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void getStatement(){
+        try {
+            this.st=conn.createStatement();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
 }

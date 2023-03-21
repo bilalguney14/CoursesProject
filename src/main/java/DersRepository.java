e10bdbde94a58621d054d77febed496077801f8e
Ebubekir <ebubekirc442@gmail.com>
2023-03-21 16:43:19 +0300

    Ebu3.repository.filtre

--- src/main/java/DersRepository.java
+++ src/main/java/DersRepository.java
@@ -1,4 +1,7 @@
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DersRepository {
 
@@ -216,9 +219,40 @@ public class DersRepository {
 
 
 
-
-
-
+    public static List<Ders> findDersByNameOrLastname(String nameOrLastname){
+        List<Ders> list=new ArrayList<>();
+        getConnection();
+        String searched="%"+nameOrLastname+"%";
+        String sql="SELECT * FROM t_ders WHERE dersAdi ILIKE ? OR ogrAdi ILIKE ?";
+        getPreparedStatement(sql);
+        try {
+            prst.setString(1,searched);
+            prst.setString(2,searched);
+            ResultSet result=prst.executeQuery();
+            while (result.next()){
+                Ders ders=new Ders();
+                ders.setDersKodu(result.getInt("dersKodu"));
+                ders.setDersAdi(result.getString("dersAdi"));
+                ders.setKredi(result.getString("kredi"));
+                ders.setOgrSayisi(result.getString("ogrSayisi"));
+                ders.setOgrAdi(result.getString("ogrAdi"));
+                list.add(ders);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }finally {
+            try {
+                prst.close();
+                conn.close();
+                private void getPreparedStatement(String sql) {
+                    try {
+                        this.prst = conn.prepareStatement(sql);
+                    } catch (SQLException e) {
+                        System.out.println(e.getMessage());
+                    }
+                }
+                return list;
+            }
 
 
 
